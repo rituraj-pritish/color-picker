99f7b512fc9f63c509f86435ae83852af15daaa0
rituraj-pritish <riturajpritish@gmail.com>
2019-09-17 13:10:44 +0530

    initial commit

---
+++ src/components/Palette.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Palette = () => {
+  return (
+    <div className='Palette'>
+      <div className='Palette-colors'>
+      </div>
+    </div>
+  )
+}
+
+export default Palette
